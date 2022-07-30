2d4426de2bae4ad9550a880feab92765f21ee47c
alex <alexrfarnes@gmail.com>
2022-07-30 13:23:43 +0800

    Configure locale

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,4 @@
+module ApplicationHelper
+    def payload_test
+        JSON.parse(response.body, symbolize_names: true) # convertir la respuesta del servidor a un objeto JSON para poder evaluar durante las pruebas
+end
